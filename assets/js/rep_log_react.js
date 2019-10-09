00f5622bca4f4588b4780f01a01a666e760bb56b
Victor <victor.du.77@hotmail.fr>
2019-10-09 09:22:21 +0200

    Course 2

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,1 +1,11 @@
-console.log('Oh hallo React peeps! ?️');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const el  = React.createElement(
+    'h2',
+    null,
+    'Lift History',
+    React.createElement('span', null, ' !!')
+);
+
+ReactDom.render(el, document.getElementById("lift-stuff-app"));
